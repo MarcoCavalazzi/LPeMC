c429e8760b4f8bdb4789e6fb5edd24bc009c5158
Marco Cavalazzi <marcocarlo.cavalazzi@studio.unibo.it>
2016-02-25 14:23:59 +0100

    small changes

--- LPeMC/src/NotNode.java
+++ LPeMC/src/NotNode.java
@@ -28,15 +28,15 @@ public class NotNode implements Node {
     @Override
     public String codeGeneration() {
         String lab1 = FOOLlib.freshLabel(); //il label per il not
-        String lab2 = FOOLlib.freshLabel();//label per il true
+        String lab2 = FOOLlib.freshLabel();	//label per il true
         return this.exp.codeGeneration()
-                + "push 1" + "\n"        //se per esempio ho 3, cioè false, ecco cosa succede: ho 3 sullo stack, metto anche 1, 
-                + "beq" + lab1 + "\n"	 //se 3 è == 1, metto 1 sullo stack cioè true, cioè il not di 3(False), altrimenti metto 0 (push 0)
-                + "push 1" +  "\n"
-                + "b " + lab2 + "\n"
-                + lab1 + ":\n"
-                + "push 0" + "\n"
-                + lab2 + ":\n";
+                + "push 1" 		+ "\n"   //se per esempio ho 3, cioè false, ecco cosa succede: ho 3 sullo stack, metto anche 1, 
+                + "beq" + lab1 	+ "\n"	 //se 3 è == 1, metto 1 sullo stack cioè true, cioè il not di 3(False), altrimenti metto 0 (push 0)
+                + "push 1" 		+ "\n"
+                + "b " + lab2 	+ "\n"
+                + lab1 + ":"	+ "\n"
+                + "push 0" 		+ "\n"
+                + lab2 + ":"	+ "\n";
     }
 
 
