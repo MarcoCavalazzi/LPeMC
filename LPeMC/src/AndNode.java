1f6a127485533270c07bee0a69b66e200c56e6cd
Marco Cavalazzi <marcocarlo.cavalazzi@studio.unibo.it>
2016-02-25 12:50:28 +0100

    Edited AndNode.java based on OrNode.java.

--- LPeMC/src/AndNode.java
+++ LPeMC/src/AndNode.java
@@ -1,22 +1,40 @@
 
 public class AndNode implements Node {
-
+	private Node left;
+	private Node right;
+	
+	public AndNode(Node l, Node r){
+		left=l;
+		right=r;
+	}
+	
 	@Override
-	public String toPrint(String indent) {
-		// TODO Auto-generated method stub
-		return null;
+	public String toPrint(String s) {
+		return s+ "OrNode["+left.toPrint(s)+","+right.toPrint(s)+"]";
 	}
 
 	@Override
 	public Node typeCheck() {
-		// TODO Auto-generated method stub
-		return null;
+		if (!(FOOLlib.isSubtype(left,new BoolTypeNode()))  ||  (FOOLlib.isSubtype(right,new BoolTypeNode()))){
+		    System.exit(0);
+		}
+		
+		return new BoolTypeNode();
 	}
 
 	@Override
 	public String codeGeneration() {
-		// TODO Auto-generated method stub
-		return null;
+		// Marco: questa è la linea di codice che ho trovato in OrNode.java.
+		// Sinceramente però non capisco perché usi "add" per fare un OR e non so bnee cosa mettere per fare l'AND.
+		return left.codeGeneration()+right.codeGeneration()+"add\n";
 	}
 
 }
+
+/*  
+	  public String codeGeneration(){
+	    return left.codeGeneration()+right.codeGeneration()+"add\n"; 
+	  }
+}
+
+*/
