646c4737ec13734048648934716e14c918f73814
Marco Cavalazzi <marcocarlo.cavalazzi@studio.unibo.it>
2016-03-03 15:53:01 +0100

    Added some comments and some small changes. The If node now displays clearly the 3 sections "if", "then", "else".

--- LPeMC/src/MinusNode.java
+++ LPeMC/src/MinusNode.java
@@ -8,25 +8,24 @@ public class MinusNode implements Node {
 	    right=r;
 	  }
 	  public String toPrint(String s) {
-		    return s+"Minus\n" + left.toPrint(s+"  ")  
-		                       + right.toPrint(s+"  ") ; 
-		  }
-		  
-		  public Node typeCheck() {
-		    if (! ( FOOLlib.isSubtype(left.typeCheck(),new IntTypeNode()) &&
-		            FOOLlib.isSubtype(right.typeCheck(),new IntTypeNode()) ) ) {
-		      System.out.println("Non integers in difference");
-		      System.exit(0);
-		    }
-		    return new IntTypeNode();
-		  }  
-		  
-		  public String codeGeneration() {
-				// aggiungiamo un'istruzione che dirà di fare push sullo stack del valore (e andiamo a capo). Notazione polacca!
-				// non possiamo giò calcolare il risultato e pushare quello sullo stack perché potremmo avere anche due espressioni (non solo due numeri banali).
-				return left.codeGeneration()+
-					   right.codeGeneration()+
-					   "sub\n";
-		  }
-	
+	    return s+"Minus\n" + left.toPrint(s+"  ")  
+	                       + right.toPrint(s+"  ") ; 
+	  }
+	  
+	  public Node typeCheck() {
+	    if (! ( FOOLlib.isSubtype(left.typeCheck(),new IntTypeNode()) &&
+	            FOOLlib.isSubtype(right.typeCheck(),new IntTypeNode()) ) ) {
+	      System.out.println("Non integers in difference");
+	      System.exit(0);
+	    }
+	    return new IntTypeNode();
+	  }  
+	  
+	  public String codeGeneration() {
+			// aggiungiamo un'istruzione che dirà di fare push sullo stack del valore (e andiamo a capo). Notazione polacca!
+			// non possiamo giò calcolare il risultato e pushare quello sullo stack perché potremmo avere anche due espressioni (non solo due numeri banali).
+			return left.codeGeneration()+
+				   right.codeGeneration()+
+				   "sub\n";
+	  }
 }
